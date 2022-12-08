ae4204862a54e0a0da91bcb23b243b0556ce5bd3
RavaliKoppu <119938446+RavaliKoppu@users.noreply.github.com>
2022-12-08 22:02:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,19 @@ import {
 } from "react-router-dom";
 
 function App() {
-  const apiKey='0acfa1f0af8149c6aa4bb005a0537224';
   return (
     <div>
       <BrowserRouter>
       <NavBar/>
       <Routes>
-      <Route path="/" element={<News apiKey={apiKey} key="general" pageSize={10} country="in" category="general"></News>}/>
-      <Route path="/business" element={<News apiKey={apiKey} key="business" pageSize={10} country="in" category="business"></News>}/>
-      <Route path="/entertainment"element={<News apiKey={apiKey}  key="entertainment" pageSize={10} country="in" category="entertainment"></News>}/>
-      <Route path="/general"element={<News apiKey={apiKey}  key="general" pageSize={10} country="in" category="general"></News>}/>
-      <Route path="/health"element={<News apiKey={apiKey}  key="health" pageSize={10} country="in" category="health"></News>}/>
-      <Route path="/science"element={<News apiKey={apiKey}  key="science" pageSize={10} country="in" category="science"></News>}/>
-      <Route path="/sports"element={<News apiKey={apiKey}  key="sports" pageSize={10} country="in" category="sports"></News>}/>
-      <Route path="/technology"element={<News apiKey={apiKey}  key="technology" pageSize={10} country="in" category="technology"></News>}/>
+      <Route path="/" element={<News apiKey="0acfa1f0af8149c6aa4bb005a0537224" key="general" pageSize={10} country="in" category="general"></News>}/>
+      <Route path="/business" element={<News apiKey="0acfa1f0af8149c6aa4bb005a0537224" key="business" pageSize={10} country="in" category="business"></News>}/>
+      <Route path="/entertainment"element={<News apiKey="0acfa1f0af8149c6aa4bb005a0537224"  key="entertainment" pageSize={10} country="in" category="entertainment"></News>}/>
+      <Route path="/general"element={<News apiKey="0acfa1f0af8149c6aa4bb005a0537224"  key="general" pageSize={10} country="in" category="general"></News>}/>
+      <Route path="/health"element={<News apiKey="0acfa1f0af8149c6aa4bb005a0537224"  key="health" pageSize={10} country="in" category="health"></News>}/>
+      <Route path="/science"element={<News apiKey="0acfa1f0af8149c6aa4bb005a0537224"  key="science" pageSize={10} country="in" category="science"></News>}/>
+      <Route path="/sports"element={<News apiKey="0acfa1f0af8149c6aa4bb005a0537224"  key="sports" pageSize={10} country="in" category="sports"></News>}/>
+      <Route path="/technology"element={<News apiKey="0acfa1f0af8149c6aa4bb005a0537224"  key="technology" pageSize={10} country="in" category="technology"></News>}/>
       </Routes>
       </BrowserRouter>
     </div>
